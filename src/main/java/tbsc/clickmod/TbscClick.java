ec00efc906b75b250cfcfd393e4cb6d1d61eaafc
Tbsc <danielben60@gmail.com>
2016-03-16 19:37:43 +0200

    Source?
    I have no idea why I need to commit it again...

--- src/main/java/tbsc/clickmod/TbscClick.java
+++ src/main/java/tbsc/clickmod/TbscClick.java
@@ -1,18 +1,6 @@
 package tbsc.clickmod;
 
-import cpw.mods.fml.client.event.ConfigChangedEvent;
-import cpw.mods.fml.client.registry.ClientRegistry;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.FMLLog;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.InputEvent;
-import cpw.mods.fml.common.gameevent.TickEvent;
-import cpw.mods.fml.relauncher.Side;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.MovingObjectPosition;
@@ -20,33 +8,45 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.FMLLog;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.relauncher.Side;
 import org.lwjgl.input.Keyboard;
 
-@Mod(modid = TbscClick.MODID, version = TbscClick.VERSION, guiFactory = TbscClick.GUI_FACTORY)
-public class TbscClick extends Gui {
+@Mod(modid = TbscClick.MODID, version = TbscClick.VERSION, guiFactory = TbscClick.GUI_FACTORY, canBeDeactivated = true)
+public class TbscClick {
 
     public static final String MODID = "TbscClick";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     public static final String GUI_FACTORY = "tbsc.clickmod.gui.TCGuiFactory";
-    public static boolean shouldClick = false;
-    public static KeyBinding keyToggle;
+    public static boolean shouldLeftClick = false;
+    public static boolean shouldRightClick = false;
+    public static KeyBinding keyToggleRight;
+    public static KeyBinding keyToggleLeft;
     public static Configuration config;
     public static float clickDelay = 0.5F;
-    public static boolean shouldLeftClick = false;
 
-    @EventHandler
+    @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         if (event.getSide() == Side.SERVER) {
             FMLLog.bigWarning("CLIENT-SIDE MOD INSTALLED ON SERVER. REMOVE FROM SERVER.");
             FMLLog.bigWarning("You are lucky I am adding a safety-check, or you'll crash.");
             FMLCommonHandler.instance().exitJava(1, false);
         }
-        FMLCommonHandler.instance().bus().register(this);
+        MinecraftForge.EVENT_BUS.register(this);
         config = new Configuration(event.getSuggestedConfigurationFile());
         syncConfig();
 
-        keyToggle = new KeyBinding("key.tbscclick.toggle", Keyboard.KEY_G, "key.categories.misc");
-        ClientRegistry.registerKeyBinding(keyToggle);
+        keyToggleRight = new KeyBinding("key.tbscclick.toggleright", Keyboard.KEY_G, "key.categories.misc");
+        keyToggleLeft = new KeyBinding("key.tbscclick.toggleleft", Keyboard.KEY_H, "key.categories.misc");
+        ClientRegistry.registerKeyBinding(keyToggleRight);
+        ClientRegistry.registerKeyBinding(keyToggleLeft);
     }
 
     public static void syncConfig() {
@@ -55,20 +55,14 @@ public class TbscClick extends Gui {
             config.load();
 
             // Read props from config
-            Property clickDelayProp = config.get(Configuration.CATEGORY_GENERAL, "clickDelay", 0.5, "Delay between auto-clicks.");
-            Property leftClickProp = config.get(Configuration.CATEGORY_GENERAL, "autoClickButton", "rightClick", "Changes what button will be auto-clicked.");
+            Property clickDelayProp = config.get(Configuration.CATEGORY_GENERAL, "clickDelay", 0.5, "Delay between auto-clicks. (Applies ONLY to RClick.)");
             clickDelay = (float) clickDelayProp.getDouble();
-            shouldLeftClick = getBooleanFromClickSide(leftClickProp.getString());
         } catch (Exception e) {
             // Exception
         } finally {
             // Save props to config
             if (config.hasChanged()) config.save();
         }
-    }
-
-    private static boolean getBooleanFromClickSide(String side) {
-        return !(side.equalsIgnoreCase("rightClick") || side.equalsIgnoreCase("rightButton") || side.equalsIgnoreCase("right")) && (side.equalsIgnoreCase("leftClick") || side.equalsIgnoreCase("leftButton") || side.equalsIgnoreCase("left"));
     }
 
     @SubscribeEvent
@@ -78,28 +72,41 @@ public class TbscClick extends Gui {
 
     @SubscribeEvent
     public void onTick(TickEvent.ClientTickEvent event) {
-        if (keyToggle.isPressed()) {
-            shouldClick = !shouldClick;
-        }
+        Minecraft minecraft = Minecraft.getMinecraft();
 
-        if (shouldClick) {
-            Minecraft minecraft = Minecraft.getMinecraft();
-            MovingObjectPosition rayTrace = minecraft.thePlayer.rayTrace(5, clickDelay);
+        if (!minecraft.isGamePaused()) {
+
+            if (keyToggleLeft.isPressed()) {
+                shouldLeftClick = !shouldLeftClick;
+            }
+            if (keyToggleRight.isPressed()) {
+                shouldRightClick = !shouldRightClick;
+            }
+
             if (shouldLeftClick) {
-                minecraft.playerController.clickBlock(rayTrace.blockX, rayTrace.blockY, rayTrace.blockZ, rayTrace.sideHit);
-            } else {
-                minecraft.playerController.onPlayerRightClick(minecraft.thePlayer, minecraft.theWorld,
-                        minecraft.thePlayer.getHeldItem(), rayTrace.blockX, rayTrace.blockY, rayTrace.blockZ,
-                        rayTrace.sideHit, rayTrace.hitVec);
+                MovingObjectPosition rayTrace = minecraft.thePlayer.rayTrace(5, clickDelay);
+                if (rayTrace.getBlockPos() != null) // Safe check
+                    minecraft.playerController.clickBlock(rayTrace.getBlockPos(), rayTrace.sideHit);
+            }
+            if (shouldRightClick) {
+                MovingObjectPosition rayTrace = minecraft.thePlayer.rayTrace(5, clickDelay);
+                if (rayTrace.getBlockPos() != null)
+                    minecraft.playerController.onPlayerRightClick(minecraft.thePlayer, minecraft.theWorld,
+                        minecraft.thePlayer.getHeldItem(), rayTrace.getBlockPos(), rayTrace.sideHit, rayTrace.hitVec);
             }
         }
     }
 
     @SubscribeEvent
-    public void onRenderOverlay(TickEvent.RenderTickEvent event) {
-        if (shouldClick) {
-            Minecraft minecraft = Minecraft.getMinecraft();
-            drawString(minecraft.fontRenderer, EnumChatFormatting.BOLD + "Auto-Clicker Enabled!", 6, 6, 0xFF0000);
+    public void onRenderGameOverlay(RenderGameOverlayEvent event) {
+        Minecraft minecraft = Minecraft.getMinecraft();
+
+        if (shouldLeftClick && shouldRightClick) {
+            minecraft.fontRendererObj.drawString(EnumChatFormatting.BOLD + "Auto-Clicking RClick + LClick", 6, 6, 0xFF0000);
+        } else if (shouldLeftClick) {
+            minecraft.fontRendererObj.drawString(EnumChatFormatting.BOLD + "Auto-Clicking LClick", 6, 6, 0x00FF00);
+        } else if (shouldRightClick) {
+            minecraft.fontRendererObj.drawString(EnumChatFormatting.BOLD + "Auto-Clicking RClick", 6, 6, 0xFF00FF);
         }
     }
 
