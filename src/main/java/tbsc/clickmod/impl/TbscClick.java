0cbf79544c2a4925448209b3df032bde849d689a
Tbsc <Tbsc@users.noreply.github.com>
2021-11-19 23:40:31 +0200

    Implement version specific part of more configurable interval

--- src/main/java/tbsc/clickmod/impl/TbscClick.java
+++ src/main/java/tbsc/clickmod/impl/TbscClick.java
@@ -10,20 +10,21 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.relauncher.Side;
 import org.lwjgl.input.Keyboard;
 import tbsc.clickmod.Compat;
 import tbsc.clickmod.IClick;
 import tbsc.clickmod.IKeyBind;
 import tbsc.clickmod.IRayTrace;
 
-@Mod(modid = TbscClick.MODID, useMetadata = true)
+@Mod(modid = TbscClick.MODID, clientSideOnly = true, useMetadata = true)
 public class TbscClick implements IClick {
 
     public static final String MODID = "tbscclick";
@@ -40,37 +41,75 @@ public class TbscClick implements IClick {
     private IKeyBind myKeyToggleHoldRight;
     private IKeyBind myKeySpeed;
 
+    private static final String KEY_TICKS_STEP = "ticksStep";
+    private static final int DEF_TICKS_STEP = 1;
+    private int ticksStepBetweenClicks = DEF_TICKS_STEP;
+
+    private static final String KEY_MAX_TICKS = "maxTicksBetweenClicks";
+    private static final int DEF_MAX_TICKS = 10;
+    private int maxTicksBetweenClicks = DEF_MAX_TICKS;
+
+    private static final String KEY_MIN_TICKS = "minTicksBetweenClicks";
+    private static final int DEF_MIN_TICKS = 1;
+    private int minTicksBetweenClicks = DEF_MIN_TICKS;
+
     private Minecraft minecraft = null;
+    public static Configuration config;
 
     private Compat compat;
 
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent event) {
-        if (event.getSide() == Side.CLIENT) {
-            compat = new Compat(this);
+        compat = new Compat(this);
 
-            MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(this);
 
-            keyToggleRight = new KeyBinding("key.tbscclick.toggleright", Keyboard.KEY_G, "key.categories.tbscclick");
-            keyToggleLeft = new KeyBinding("key.tbscclick.toggleleft", Keyboard.KEY_H, "key.categories.tbscclick");
-            keyToggleSmartAttack = new KeyBinding("key.tbscclick.togglesmartattack", Keyboard.KEY_V, "key.categories.tbscclick");
-            keyToggleHoldRight = new KeyBinding("key.tbscclick.toggleholdright", Keyboard.KEY_B, "key.categories.tbscclick");
-            keySpeed = new KeyBinding("key.tbscclick.speed", Keyboard.KEY_N, "key.categories.tbscclick");
+        keyToggleRight = new KeyBinding("key.tbscclick.toggleright", Keyboard.KEY_G, "key.categories.tbscclick");
+        keyToggleLeft = new KeyBinding("key.tbscclick.toggleleft", Keyboard.KEY_H, "key.categories.tbscclick");
+        keyToggleSmartAttack = new KeyBinding("key.tbscclick.togglesmartattack", Keyboard.KEY_V, "key.categories.tbscclick");
+        keyToggleHoldRight = new KeyBinding("key.tbscclick.toggleholdright", Keyboard.KEY_B, "key.categories.tbscclick");
+        keySpeed = new KeyBinding("key.tbscclick.speed", Keyboard.KEY_N, "key.categories.tbscclick");
 
-            ClientRegistry.registerKeyBinding(keyToggleRight);
-            ClientRegistry.registerKeyBinding(keyToggleLeft);
-            ClientRegistry.registerKeyBinding(keyToggleSmartAttack);
-            ClientRegistry.registerKeyBinding(keyToggleHoldRight);
-            ClientRegistry.registerKeyBinding(keySpeed);
+        ClientRegistry.registerKeyBinding(keyToggleRight);
+        ClientRegistry.registerKeyBinding(keyToggleLeft);
+        ClientRegistry.registerKeyBinding(keyToggleSmartAttack);
+        ClientRegistry.registerKeyBinding(keyToggleHoldRight);
+        ClientRegistry.registerKeyBinding(keySpeed);
 
-            minecraft = Minecraft.getMinecraft();
+        minecraft = Minecraft.getMinecraft();
 
-            myKeyUse = new KeyBind(minecraft.gameSettings.keyBindUseItem);
-            myKeyToggleRight = new KeyBind(keyToggleRight);
-            myKeyToggleLeft = new KeyBind(keyToggleLeft);
-            myKeyToggleSmartAttack = new KeyBind(keyToggleSmartAttack);
-            myKeyToggleHoldRight = new KeyBind(keyToggleHoldRight);
-            myKeySpeed = new KeyBind(keySpeed);
+        myKeyUse = new KeyBind(minecraft.gameSettings.keyBindUseItem);
+        myKeyToggleRight = new KeyBind(keyToggleRight);
+        myKeyToggleLeft = new KeyBind(keyToggleLeft);
+        myKeyToggleSmartAttack = new KeyBind(keyToggleSmartAttack);
+        myKeyToggleHoldRight = new KeyBind(keyToggleHoldRight);
+        myKeySpeed = new KeyBind(keySpeed);
+
+        config = new Configuration(event.getSuggestedConfigurationFile());
+        try {
+            processConfig();
+        } catch (Exception e) {
+            event.getModLog().error("Failed loading config");
+        } finally {
+            if (config.hasChanged()) {
+                config.save();
+            }
+        }
+    }
+
+    public void processConfig() {
+        ticksStepBetweenClicks = config.get(Configuration.CATEGORY_GENERAL, KEY_TICKS_STEP, DEF_TICKS_STEP,
+                "When changing auto click interval, by how much should it change", 1, 9999999).getInt();
+        maxTicksBetweenClicks = config.get(Configuration.CATEGORY_GENERAL, KEY_MAX_TICKS, DEF_MAX_TICKS,
+                "The maximal auto click interval possible before looping back to the minimum", 1, 9999999).getInt();
+        minTicksBetweenClicks = config.get(Configuration.CATEGORY_GENERAL, KEY_MIN_TICKS, DEF_MIN_TICKS,
+                "The minimal auto click interval", 1, 9999999).getInt();
+    }
+
+    @SubscribeEvent
+    public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+        if (event.getModID().equals(MODID)) {
+            processConfig();
         }
     }
 
@@ -166,6 +205,21 @@ public class TbscClick implements IClick {
     @Override
     public IKeyBind getSpeedKey() {
         return myKeySpeed;
+    }
+
+    @Override
+    public int getTicksStepBetweenClicks() {
+        return ticksStepBetweenClicks;
+    }
+
+    @Override
+    public int getMaxTicksBetweenClicks() {
+        return maxTicksBetweenClicks;
+    }
+
+    @Override
+    public int getMinTicksBetweenClicks() {
+        return minTicksBetweenClicks;
     }
 
     @Override
